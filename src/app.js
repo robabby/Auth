cd3246f66dfdcb2113c5382ae4bcf8e369d7852f
Rob <robabby23@gmail.com>
2017-05-11 16:48:40 -0500

    Implemented login form and clearing state

--- src/app.js
+++ src/app.js
@@ -7,12 +7,12 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
   componentWillMount() {
     firebase.initializeApp({
-      apiKey: 'AIzaSyDR_SJC1CfwLfNIL0yY0dTlJrWxV2znNjo',
-      authDomain: 'auth-9c3b4.firebaseapp.com',
-      databaseURL: 'https://auth-9c3b4.firebaseio.com',
-      projectId: 'auth-9c3b4',
-      storageBucket: 'auth-9c3b4.appspot.com',
-      messagingSenderId: '345287320414'
+      apiKey: "AIzaSyDR_SJC1CfwLfNIL0yY0dTlJrWxV2znNjo",
+      authDomain: "auth-9c3b4.firebaseapp.com",
+      databaseURL: "https://auth-9c3b4.firebaseio.com",
+      projectId: "auth-9c3b4",
+      storageBucket: "auth-9c3b4.appspot.com",
+      messagingSenderId: "345287320414"
     })
   }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
   state = {
     email: '',
     password: '',
-    error: ''
+    error: '',
+    loading: false
   }
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    firebase.auth.signInWithEmailAndPassword(email, password)
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication failed' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    })
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authentication failed',
+      loading: false
+    })
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log In
+      </Button>
+    );
   }
 
   render() {
@@ -49,9 +80,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log In
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
