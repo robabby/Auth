879577b844514d423ecb6b55ea70f54a973e89fc
Rob <robabby23@gmail.com>
2017-05-11 16:12:51 -0500

    Implemented all fail cases of login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
   state = {
-    email: ''
+    email: '',
+    password: '',
+    error: ''
+  }
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    firebase.auth.signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            this.setState({ error: 'Authentication failed' });
+          });
+      });
   }
 
   render() {
@@ -17,15 +33,36 @@ class LoginForm extends Component {
             onChangeText={email => this.setState({ email })}
           />
         </CardSection>
+
         <CardSection>
+          <Input
+            secureTextEntry
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
